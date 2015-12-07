91572aacc6f4640cf3a2e3c70fa447aef63c43bb
Tim Allen <tim@noblesamurai.com>
2015-12-07 15:09:24 +1100

    Clean up db-destroy-create.js

--- db-destroy-create.js
+++ db-destroy-create.js
@@ -1,6 +1,11 @@
 if (!process.env.NODE_ENV === 'test') process.exit(1);
 
 require('pg-destroy-create-db')(process.env.DATABASE_URL)
-  .destroyCreate( function( error ){
+  .destroyCreate( function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log('db bounced...');
     process.exit();
-})
+});
